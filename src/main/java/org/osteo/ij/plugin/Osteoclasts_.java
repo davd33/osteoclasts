d95366ac9a5713e36a4e198ab443e9289ea895ec
davd33 <davd33@davd33-pc>
2013-07-19 12:42:30 +0200

    add window

--- src/main/java/org/osteo/ij/plugin/Osteoclasts_.java
+++ src/main/java/org/osteo/ij/plugin/Osteoclasts_.java
@@ -12,9 +12,15 @@ import ij.plugin.PlugIn;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
@@ -33,6 +39,8 @@ public class Osteoclasts_ implements PlugInFilter {
 
     @Override
     public void run(ImageProcessor ip) {
+        runMiniWin();
+        
         if (imp.getOverlay() != null) {
             imp.getOverlay().setFillColor(Color.GREEN);
             imp.getOverlay().drawBackgrounds(true);
@@ -69,6 +77,54 @@ public class Osteoclasts_ implements PlugInFilter {
             public void mouseExited(MouseEvent me) {
             }
         });
+    }
+    
+    private static final int WIN_N_ACTIONS = 5;
+    
+    private enum Actions {
+        PA("Analyze Particles"),
+        OPEN("Open Images"),
+        CLASS("Run Classifier"),
+        OVERLAYS("Get Overlays"),
+        OPTIONS("Options");
+        
+        private String name;
+        
+        public String getName() {
+            return this.name;
+        }
+        
+        Actions(String name) {
+            this.name = name;
+        }
+    }
+    
+    private void runMiniWin() {
+        miniWin = new JFrame();
+        miniWin.setVisible(true);
+        
+        JButton[] actionsButtons = new JButton[this.WIN_N_ACTIONS];
+        JPanel actionsPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.VERTICAL;
+        
+        ActionListener actionListener = new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                JButton source = (JButton) ae.getSource();
+                
+                
+            }
+        };
+        
+        for (Actions action : Actions.values()) {
+            JButton actionButton = new JButton(action.getName());
+            actionsPanel.add(actionButton, c);
+            actionButton.addActionListener(actionListener);
+        }
+        
+        miniWin.getContentPane().add(actionsPanel);
+        miniWin.pack();
     }
 
     private void log(String message) {
