4def272e7ea590a5c1d135ae730e7274c29c33fa
davd33 <davd33@gmail.com>
2013-07-23 15:30:58 +0200

    convert to mask the overlay - add particle analysis plus some java documentation

--- src/main/java/org/osteo/ij/plugin/Osteoclasts_.java
+++ src/main/java/org/osteo/ij/plugin/Osteoclasts_.java
@@ -22,6 +22,7 @@ import ij.process.ColorProcessor;
 import ij.process.ImageProcessor;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -30,50 +31,49 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
- * Supply all the features for osteoclast images classification 
- * and segmentation.
+ * Supply all the features for osteoclast images classification and
+ * segmentation.
  *
  * @author davidr
  */
 public class Osteoclasts_ implements PlugInFilter {
 
     /**
-     * Main Image.
-     * Does not handle stacks yet.
+     * Main Image. Does not handle stacks yet.
      */
     private ImagePlus imp;
-    
     /**
      * Little window with a few buttons.
      */
     private JFrame miniWin;
-    
+    private JPanel miniWinInfosPanel;
     /**
      * One unique color for managing overlays.
      */
     public static final Color OVERLAY_COLOR = Color.MAGENTA;
-    
     /**
      * Simulate infinity.
      */
     protected final static double INFINITY = 1.0 / 0.0;
 
     /**
-     * The plugin is given an image.
-     * It does not manage with stacks yet.
-     * 
+     * The plugin is given an image. It does not manage with stacks yet.
+     *
      * @param arg useless for now
      * @param imp
-     * @return 
+     * @return
      */
     @Override
     public int setup(String arg, ImagePlus imp) {
@@ -83,12 +83,13 @@ public class Osteoclasts_ implements PlugInFilter {
 
     /**
      * Run the osteoclast counting plugin.
-     * 
-     * @param ip 
+     *
+     * @param ip
      */
     @Override
     public void run(ImageProcessor ip) {
         runMiniWin();
+        logToMiniWin("Welcome Ana! ;)");
 
         if (imp.getOverlay() != null) {
             imp.getOverlay().setFillColor(Osteoclasts_.OVERLAY_COLOR);
@@ -122,15 +123,13 @@ public class Osteoclasts_ implements PlugInFilter {
             public void windowDeactivated(WindowEvent we) {
             }
         });
-        
+
         this.imp.getCanvas().addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent me) {
                 if (me.isControlDown() && impML.getOverlay() != null && impML.getRoi() != null) {
                     impML.getOverlay().add(impML.getRoi());
-                    impML.getOverlay().setFillColor(Color.GREEN);
-                    impML.getOverlay().drawBackgrounds(true);
-                    impML.getOverlay().drawLabels(true);
+                    impML.getOverlay().setFillColor(Osteoclasts_.OVERLAY_COLOR);
                 }
             }
 
@@ -153,8 +152,7 @@ public class Osteoclasts_ implements PlugInFilter {
     }
 
     /**
-     * Several actions that the plugin 
-     * allows the user to do.
+     * Several actions that the plugin allows the user to do.
      */
     private enum Actions {
 
@@ -186,6 +184,21 @@ public class Osteoclasts_ implements PlugInFilter {
             miniWin.setVisible(false);
         }
         miniWin = null;
+    }
+
+    /**
+     * Display little message into little window.
+     *
+     * @param message
+     */
+    private void logToMiniWin(final String message) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Osteoclasts_.this.miniWinInfosPanel.add(new JLabel(message));
+                Osteoclasts_.this.miniWinInfosPanel.updateUI();
+                Osteoclasts_.this.miniWin.pack();
+            }
+        });
     }
 
     /**
@@ -222,6 +235,9 @@ public class Osteoclasts_ implements PlugInFilter {
             actionButton.addActionListener(actionListener);
         }
 
+        miniWinInfosPanel = new JPanel();
+
+        miniWin.getContentPane().add(miniWinInfosPanel, BorderLayout.NORTH);
         miniWin.getContentPane().add(actionsPanel, BorderLayout.CENTER);
         miniWin.pack();
     }
@@ -234,43 +250,37 @@ public class Osteoclasts_ implements PlugInFilter {
     }
 
     /**
-     * Get the overlays.
-     * Analyze probability images, which 
-     * are the results of the classification.
+     * Get the overlays. Analyze probability images, which are the results of
+     * the classification.
      */
     private void overlays() {
         System.out.println("overlays");
     }
 
     /**
-     * After classification is corrected (by hand),
-     * runs the particle analysis and save the 
-     * results in a csv file.
+     * After classification is corrected (by hand), runs the particle analysis
+     * and save the results in a csv file.
      */
     private void pa() {
         Overlay o = imp.getOverlay();
         o.drawLabels(false);
         o.drawNames(false);
         o.drawBackgrounds(true);
-        IJ.log(o.size() + " detected osteoclasts");
 
         RoiManager rm = new RoiManager(true);
         for (int i = 0; i < o.size(); i++) {
             rm.addRoi(o.get(i));
         }
 
-        IJ.log("" + rm.getCount());
-
         byte[] maskBytes = new byte[imp.getWidth() * imp.getHeight()];
         Arrays.fill(maskBytes, (byte) 255);
         ImagePlus mask = new ImagePlus("mask", new ByteProcessor(imp.getWidth(), imp.getHeight(), maskBytes));
-        mask.show();
 
         for (Roi roi : rm.getRoisAsArray()) {
             mask.getProcessor().setColor(Color.BLACK);
             mask.getProcessor().fill(roi);
         }
-        
+
         // analyze particles
         ResultsTable paResults = new ResultsTable();
         ParticleAnalyzer particleAnalyzer = new ParticleAnalyzer(
@@ -283,9 +293,23 @@ public class Osteoclasts_ implements PlugInFilter {
                 | Measurements.SHAPE_DESCRIPTORS,
                 paResults,
                 0d, INFINITY, 0d, 1d);
-        particleAnalyzer.analyze(imp);
+        particleAnalyzer.analyze(mask);
         try {
-            paResults.saveAs(IJ.getFilePath("Where should the results be saved?"));
+            String path = IJ.getFilePath("Where should the results be saved?");
+            if (path != null) {
+                File file = new File(path);
+
+                if (file.exists()) {
+                    boolean nonetheless = IJ.showMessageWithCancel(
+                            "Save results...",
+                            "\"" + file.getName() + "\" already exists.\nDo you want to replace it?");
+                    if (nonetheless) {
+                        paResults.saveAs(path);
+                    }
+                } else {
+                    paResults.saveAs(path);
+                }
+            }
         } catch (IOException ex) {
             IJ.log("Unable to save the results file.");
             log(ex.getMessage());
@@ -293,12 +317,13 @@ public class Osteoclasts_ implements PlugInFilter {
     }
 
     /**
-     * Color threshold.
-     * It's not working well...
-     * 
+     * Color threshold. It's not working well... It actually is under
+     * implementation... :D Since I don't need it for the plugin, I won't
+     * continue developing this method until I have to.
+     *
      * @param impLocal
      * @param color
-     * @return 
+     * @return
      */
     private ImagePlus threshold(ImagePlus impLocal, Color color) {
         int[] min = new int[3];
@@ -346,8 +371,8 @@ public class Osteoclasts_ implements PlugInFilter {
 
     /**
      * Log message into console.
-     * 
-     * @param message 
+     *
+     * @param message
      */
     private void log(String message) {
         System.out.println(message);
