9f4b9c3a5662dd1abcd8a40cdad88f115c2395ca
davd33 <davd33@gmail.com>
2013-07-31 11:44:38 +0200

    fscores

--- scripts/r/fscores.r
+++ scripts/r/fscores.r
@@ -1,9 +1,44 @@
-setwd("~/data2/davdNewImages/Benchmark/csv/anasparamsa")
-fscores<-read.csv("fscores.csv", sep=",")
-fscores$recall<-fscores$tp / fscores$tp.fn
-fscores$fn<-(fscores$tp / fscores$recall) - fscores$tp
-fscores$precision<-fscores$tp / (fscores$tp + fscores$fp)
+compute_fscores <- function () {
+  results<-data.frame();
+  
+  for (i in list.files()) {
+    filestr<-paste(toString(i),"/fscores.csv", sep="");
+    
+    if (file.exists(filestr) && !file.info(filestr)$isdir) {
+      fscores<-read.csv(filestr, sep=",");
+      
+      fscores$recall<-fscores$tp / fscores$tp.fn;
+      fscores$fn<-fscores$tp.fn - fscores$tp;
+      fscores$precision<-fscores$tp / (fscores$tp + fscores$fp);
+      
+      # delete NaN values
+      fscores<-fscores[complete.cases(fscores),];
+      # remove recalls above one
+      fscores<-fscores[fscores$recall <= 1,];
+      
+      recall<-mean(fscores$recall);
+      precision<-mean(fscores$precision);
+      maxRecall<-max(fscores$recall);
+      minRecall<-min(fscores$recall);
+      minPrecision<-min(fscores$precision);
+      maxPrecision<-max(fscores$precision);
+      
+      results<-rbind(results, data.frame(
+        recall=recall, 
+        precision=precision,
+        max_recall=maxRecall,
+        min_recall=minRecall,
+        max_precision=maxPrecision,
+        min_precision=minPrecision
+      ));
+    }
+  }
+  
+  results<-results[order(-results$recall, -results$precision),]
+  return(results);
+}
 
-recall<-mean(fscores$recall)
-precision<-mean(fscores$precision)
-summary<-data.frame(recall=recall, precision=precision)
+setwd("~/data2/davdNewImages/Benchmark/csv/p_space_assess/")
+r<-compute_fscores();
+
+write.table(r, file="fscores.csv", sep=",");
